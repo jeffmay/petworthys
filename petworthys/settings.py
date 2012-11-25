6055e802c5ebf61e89cf9042547452d97225d426
Jeff May <jeff.may@audaxhealth.com>
2012-11-25 00:52:32 -0500

    Added fonts and sample page. Updated settings.

--- petworthys/settings.py
+++ petworthys/settings.py
@@ -59,7 +59,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = join(PROJECT_ROOT, 'assets/collected_static')
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
@@ -70,6 +70,7 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    join(PROJECT_ROOT, 'assets/static'),
 )
 
 # List of finder classes that know how to find static files in
@@ -109,7 +110,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    join(PROJECT_ROOT, "templates")
+    join(PROJECT_ROOT, "templates"),
 )
 
 INSTALLED_APPS = (
